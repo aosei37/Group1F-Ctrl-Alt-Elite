85ae9b794603761f18a992f8533d88441be186aa
aosei37 <aosei12@terpmail.umd.edu>
2021-10-08 14:59:17 +0000

    D1 - Initial layout for data collection script

---
+++ data_collection.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#1. Collect the information from nohup in MITM
+#2. Track changes within the system and output to directory, copy to host
+#3. Copy directory with files uploaded with poisoned commands to host
+#4. Periodically capture system health (container ram) and output to directory, copy to host
